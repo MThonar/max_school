d22088455deb2fd09f98512dff61b54885de79cd
mthonar <maxime.thonar@gmail.com>
2021-12-13 21:25:55 +0100

    statistic_target

--- joinestimation.c
+++ joinestimation.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define ARRAY_LENGTH 5
+#define STATISTIC_TARGET 5
 
 int array_lower1[ARRAY_LENGTH] = {0, 2, 7, 19, 22};
 int array_upper1[ARRAY_LENGTH] = {14, 9, 15, 27, 30};
@@ -11,8 +12,6 @@ int array_lower2[ARRAY_LENGTH] = {0, 2, 10, 22, 30};
 int array_upper2[ARRAY_LENGTH] = {6, 13, 19, 30, 35};
 int array_lower_tri2[ARRAY_LENGTH] = {0, 2, 10, 22, 30};
 int array_upper_tri2[ARRAY_LENGTH] = {6, 13, 19, 30, 35};
-
-int statistic_target = 5; 		//number of buckets
 
 float	*ft_equiwidth_freq(int *array_lower, int *array_upper, int *array_lower_tri, int *array_upper_tri)
 {
@@ -27,16 +26,16 @@ float	*ft_equiwidth_freq(int *array_lower, int *array_upper, int *array_lower_tr
 	float	increment;
 
 	hist_length = array_upper_tri[ARRAY_LENGTH - 1] - array_lower_tri[0];
-	bucket_length = (float)hist_length / (float)statistic_target;
+	bucket_length = (float)hist_length / (float)STATISTIC_TARGET;
 	lowest_bound = array_lower_tri[0];
 	lower_bucket_bound = lowest_bound;
 	upper_bucket_bound = lower_bucket_bound + bucket_length;
-	bucket = malloc(sizeof(float) * statistic_target);
-	
+	bucket = malloc(sizeof(float) * STATISTIC_TARGET);
+
 	if (!bucket)
 		return NULL;
 
-	for (i = 0; i < statistic_target; i++)
+	for (i = 0; i < STATISTIC_TARGET; i++)
 	{
 		for(j = 0; j < ARRAY_LENGTH; j++)
 		{
@@ -51,11 +50,11 @@ float	*ft_equiwidth_freq(int *array_lower, int *array_upper, int *array_lower_tr
 				bucket[i] += increment;
 			}
 			else if((array_lower[j] <= lower_bucket_bound) && (array_upper[j] >= upper_bucket_bound))
-			{;			
+			{;
 				bucket[i] += 1 ;
 			}
 			else if((array_upper[j] < upper_bucket_bound) && (array_upper[j] > lower_bucket_bound))
-			{		
+			{
 				increment = (array_upper[j] - lower_bucket_bound) / bucket_length;
 				bucket[i] += increment ;
 			}
@@ -90,15 +89,15 @@ float multiplication(float* bucket1, float* bucket2)
 	max_hist_value2 = array_upper_tri2[ARRAY_LENGTH - 1];
 	hist_length1 = max_hist_value1 - min_hist_value1;
 	hist_length2 = max_hist_value2 - min_hist_value2;
-	bucket_length1 = (float)hist_length1 / (float)statistic_target;
-	bucket_length2 = (float)hist_length2 / (float)statistic_target;
+	bucket_length1 = (float)hist_length1 / (float)STATISTIC_TARGET;
+	bucket_length2 = (float)hist_length2 / (float)STATISTIC_TARGET;
 	lower_bucket_bound1 = min_hist_value1;
 	upper_bucket_bound1 = lower_bucket_bound1 + bucket_length1;
-	for (i = 0; i < statistic_target; i++)
+	for (i = 0; i < STATISTIC_TARGET; i++)
 	{
 		lower_bucket_bound2 = min_hist_value2;
 		upper_bucket_bound2 = lower_bucket_bound2 + bucket_length2;
-		for(j = 0; j < statistic_target; j++)
+		for(j = 0; j < STATISTIC_TARGET; j++)
 		{
 			if( (lower_bucket_bound1 <= lower_bucket_bound2) && (upper_bucket_bound1 >= lower_bucket_bound2) )
 				resultat += bucket1[i]*bucket2[j];
@@ -115,10 +114,10 @@ float multiplication(float* bucket1, float* bucket2)
 }
 
 float moyenne(float* bucket)
-{	
+{
 	float moyenne = 0;
 	int i;
-	for (i = 0; i < statistic_target; i++){
+	for (i = 0; i < STATISTIC_TARGET; i++){
 		//printf("\nmoyenne = %f\n", moyenne);
 		moyenne += bucket[i];
 		}
@@ -134,12 +133,12 @@ int	main(void)
 	float *bucket2;
 	bucket1 = ft_equiwidth_freq(array_lower1, array_upper1, array_lower_tri1, array_upper_tri1);
 	printf("bucket1 = ( ");
-	for(i = 0; i < statistic_target; i++)
+	for(i = 0; i < STATISTIC_TARGET; i++)
 		printf("%f ", bucket1[i]);
 	printf(")\n");
 	bucket2 = ft_equiwidth_freq(array_lower2, array_upper2, array_lower_tri2, array_upper_tri2);
 	printf("bucket2 = ( ");
-	for(i = 0; i < statistic_target; i++)
+	for(i = 0; i < STATISTIC_TARGET; i++)
 		printf("%f ", bucket2[i]);
 	printf(")\n");
 	float resultat;
