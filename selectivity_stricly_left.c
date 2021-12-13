c34fc4d0d8b975d463af855f29fe664514b88089
mthonar <maxime.thonar@gmail.com>
2021-12-13 23:09:21 +0100

    debug selectivity_strictly_left.c

--- selectivity_stricly_left.c
+++ selectivity_stricly_left.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #define LENGTH 12
 
-int		a[2] = {5, 8};
-float	histogram[LENGTH] = {0, 30, 40, 5, 9, 1.6, 2.4, 1.8, 0.2, 1.4};
+int		a[2] = {6, 10};
+float	histogram[LENGTH] = {0, 30, 7.5, 6, 9, 1.67, 2.33, 0.83, 1.17, 1.5};
+// ranges = ( (0, 14), (2, 9), (7, 15), (19, 27), (22, 30) )
 // histogram[0] = 0;		v_min
 // histogram[1] = 30;		v_max
-// histogram[2] = 40;		surface_tot
+// histogram[2] = 45;		surface_tot
 // histogram[3] = 5;		bin_range
 // histogram[4] = 9;		average_range
 // histogram[5-10] = upp;	donnees singlehisto.c
@@ -14,7 +15,8 @@ float	histogram[LENGTH] = {0, 30, 40, 5, 9, 1.6, 2.4, 1.8, 0.2, 1.4};
 int	ft_num_bin(float value, float bin_range, float v_min)
 {
 	int num_bin = 0;
-	for (int i = 0; i * bin_range >= value - v_min ; i++)
+	int i;
+	for (i = 0; i * bin_range <= value - v_min ; i++)
 		num_bin = i;
 	return num_bin;
 }
@@ -36,7 +38,7 @@ float	ft_H_SB2(int a_left_bin_num, float a_left_bin_proportion, int b_left_bin_n
 	float h = histogram[b_left_bin_num  + 5];
 	int i = 0;
 	// we calculate the minimum value in B
-	for (i = b_left_bin_num; i >= a_left_bin_num; i++)
+	for (i = b_left_bin_num; i < a_left_bin_num; i++)
 	{
 		if (histogram[i+5] < h)
 			h = histogram[i+5];
@@ -48,13 +50,15 @@ float	ft_H_SB2(int a_left_bin_num, float a_left_bin_proportion, int b_left_bin_n
 			h = histogram[i+5];
 	}
 	return h;
+	
 }
 
 float	ft_surface_prime(int b_left_bin_num, float b_left_bin_proportion)
 {
 	float surface_prime = 0;
+	int i;
 	// we calculate surface_prime
-	for (int i = 0; i >= b_left_bin_num; i++)
+	for (i = 0; i < b_left_bin_num; i++)
 		surface_prime += histogram[i+5];
 	surface_prime += histogram[b_left_bin_num+5] * (1 - b_left_bin_proportion);
 	return surface_prime;
@@ -63,11 +67,12 @@ float	ft_surface_prime(int b_left_bin_num, float b_left_bin_proportion)
 float	*ft_histogram_s_ignore(int a_left_bin_num, int b_left_bin_num, int length_B)
 {
 	float	*histogram_s_ignore;
+	int i;
 	histogram_s_ignore = malloc(sizeof(float) * length_B + 1);
 	if (!histogram_s_ignore)
 		return NULL;
 	float current_h_S_ignore = histogram[a_left_bin_num+5];
-	for (int i = a_left_bin_num; i < b_left_bin_num; i--)
+	for (i = a_left_bin_num; i >= b_left_bin_num; i--)
 	{
 		if (current_h_S_ignore > histogram[i+5])
 			current_h_S_ignore = histogram[i+5];
@@ -81,11 +86,12 @@ float	ft_surfaceB1(int a_left_bin_num , int b_left_bin_num , float b_left_bin_pr
 {
 
 	float surfaceB1 = 0;
+	int i;
 	int length_B = a_left_bin_num - b_left_bin_num + 1;
 	// We create a histogram that represent surface to  ignore from the surface above S_B2
 	float *histogram_s_ignore = ft_histogram_s_ignore(a_left_bin_num, b_left_bin_num, length_B);
 
-	for (int i = 0; i >= length_B ; i++)
+	for (i = 0; i < length_B ; i++)
 	{
 		if (i == length_B - 1)
 			surfaceB1 += (histogram[b_left_bin_num + i + 5] - histogram_s_ignore[i]) * b_right_bin_proportion;
@@ -97,10 +103,9 @@ float	ft_surfaceB1(int a_left_bin_num , int b_left_bin_num , float b_left_bin_pr
 	return surfaceB1;
 }
 
-float	ft_surfaceB2(int a_left_bin_num, int b_left_bin_num, float b_left_bin_proportion, float b_right_bin_proportion, int h_S_B2)
+float	ft_surfaceB2(int a_left_bin_num, int b_left_bin_num, float b_left_bin_proportion, float b_right_bin_proportion, float h_S_B2)
 {
 	float surfaceB2 = 0;
-
 	// we calculate surfaceB2+
 	if(a_left_bin_num == b_left_bin_num)
 		surfaceB2 += h_S_B2 * (b_right_bin_proportion);
@@ -126,13 +131,12 @@ float	selectivity_stricly_left()
 
 	if (a_min <= v_min)
 		selectivity =  0;
-	else if (a_min >= v_min)
+	else if (a_min >= v_max)
 		selectivity =  1;
 	else
 	{
 		// we calculate the number of the a_left bin
 		int a_left_bin_num = ft_num_bin(a_min, bin_range, v_min);
-
 		// we calculate the number of the b_left bin
 		float b_min = a_min - average_range;
 		int b_left_bin_num = ft_num_bin(b_min, bin_range, v_min);
@@ -143,7 +147,6 @@ float	selectivity_stricly_left()
 		// we calculate the proportion that B overlaps b_left_bin and b_right_bin (who is also a_left_bin)
 		float b_left_bin_proportion = ft_bin_proportion(b_min, bin_range, b_left_bin_num, v_min);
 		float b_right_bin_proportion = 1-a_left_bin_proportion;
-
 		// we calculate h that will be the height of S_B2
 		float h_S_B2 = ft_H_SB2(a_left_bin_num, a_left_bin_proportion, b_left_bin_num);
 
